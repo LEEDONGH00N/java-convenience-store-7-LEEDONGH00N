26b41d21a03b1f05219a6f6fea950c5ee2d86040
LeeDongHoon <likelion.leedh@gmail.com>
2024-11-11 18:45:44 +0900

    [FEAT] 멤버십 할인 관련 기능 및 상수 추가

---
+++ src/main/java/store/utils/MembershipUtils.java
@@ -0,0 +1,19 @@
+package store.utils;
+
+import store.properties.MembershipProperties;
+
+public class MembershipUtils {
+
+    public static int discount(int amountWithoutPromotion){
+        int discountAmount = amountWithoutPromotion -
+                (amountWithoutPromotion * MembershipProperties.MEMBERSHIP_DISCOUNT_PERCENTAGE / 100);
+        return validateDiscountAmountMax(discountAmount);
+    }
+
+    private static int validateDiscountAmountMax(int discountAmount){
+        if(discountAmount >= MembershipProperties.MEMBERSHIP_DISCOUNT_MAXIMUM_AMOUNT){
+            return MembershipProperties.MEMBERSHIP_DISCOUNT_MAXIMUM_AMOUNT;
+        }
+        return discountAmount;
+    }
+}
